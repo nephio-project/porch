875fe6ddc105431ea145f0c90a86781f97820702
Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>
2024-03-05 17:01:40 +0000

    update pull and push commands
    Signed-off-by: Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>

--- pkg/cli/commands/rpkg/pull/command.go
+++ pkg/cli/commands/rpkg/pull/command.go
@@ -127,7 +127,7 @@ func (r *runner) runE(cmd *cobra.Command, args []string) error {
 			return errors.E(op, err)
 		}
 	}
-	fmt.Fprintf(cmd.OutOrStdout(), "Package: %s pulled successfully\n", packageName)
+	fmt.Fprintf(cmd.OutOrStdout(), "Package: %s pulled\n", packageName)
 	return nil
 }
 
