4d5355fb7774ca658e2d0fe3e625630e2418b249
Fiachra Corcoran <fiachra.corcoran@est.tech>
2024-03-26 17:19:17 +0000

    Fix missing headers from controller yaml

--- controllers/packagevariants/pkg/controllers/packagevariant/packagevariant_controller.go
+++ controllers/packagevariants/pkg/controllers/packagevariant/packagevariant_controller.go
@@ -63,7 +63,7 @@ const (
 	requeueDuration = 30 * time.Second
 )
 
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.14.0 rbac:roleName=porch-controllers-packagevariants webhook paths="." output:rbac:artifacts:config=../../../config/rbac
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@v0.14.0 rbac:headerFile=../../../../../scripts/boilerplate.yaml.txt,roleName=porch-controllers-packagevariants webhook paths="." output:rbac:artifacts:config=../../../config/rbac
 
 //+kubebuilder:rbac:groups=config.porch.kpt.dev,resources=packagevariants,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=config.porch.kpt.dev,resources=packagevariants/status,verbs=get;update;patch
