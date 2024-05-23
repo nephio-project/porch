e7d458e5835dd19a9e88127f1c77cdd579a58350
Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>
2024-05-23 16:03:03 +0000

    remove added rbac

--- controllers/packagevariantsets/pkg/controllers/packagevariantset/packagevariantset_controller.go
+++ controllers/packagevariantsets/pkg/controllers/packagevariantset/packagevariantset_controller.go
@@ -73,7 +73,6 @@ const (
 //+kubebuilder:rbac:groups=config.porch.kpt.dev,resources=packagevariantsets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=config.porch.kpt.dev,resources=packagevariantsets/finalizers,verbs=update
 //+kubebuilder:rbac:groups=config.porch.kpt.dev,resources=packagevariants,verbs=create;delete;get;list;patch;update;watch
-//+kubebuilder:rbac:groups=porch.kpt.dev,resources=packagerevisions,verbs=get;list;watch
 //+kubebuilder:rbac:groups=*,resources=*,verbs=list
 
 // Reconcile implements the main kubernetes reconciliation loop.
