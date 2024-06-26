edf14f357e2269c043483057458ea8b23dc15445
Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>
2024-06-26 14:05:00 +0000

    Revert "remove approval.go"
    This reverts commit a14e99119f1abadb06a1b4262634c935adb42c90.

---
+++ internal/kpt/util/porch/approval.go
@@ -0,0 +1,46 @@
+// Copyright 2022 The kpt and Nephio Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package porch
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/nephio-project/porch/api/porch/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func UpdatePackageRevisionApproval(ctx context.Context, client client.Client, key client.ObjectKey, new v1alpha1.PackageRevisionLifecycle) error {
+
+	var pr v1alpha1.PackageRevision
+	if err := client.Get(ctx, key, &pr); err != nil {
+		return err
+	}
+
+	switch lifecycle := pr.Spec.Lifecycle; lifecycle {
+	case v1alpha1.PackageRevisionLifecycleProposed, v1alpha1.PackageRevisionLifecycleDeletionProposed:
+		// ok
+	case new:
+		// already correct value
+		return nil
+	default:
+		return fmt.Errorf("cannot change approval from %s to %s", lifecycle, new)
+	}
+
+	// Approve - change the package revision kind to "final".
+	pr.Spec.Lifecycle = new
+
+	return client.Update(ctx, &pr)
+}
--- pkg/cli/commands/rpkg/reject/command.go
+++ pkg/cli/commands/rpkg/reject/command.go
@@ -96,8 +96,10 @@ func (r *runner) runE(_ *cobra.Command, args []string) error {
 		}
 		switch pr.Spec.Lifecycle {
 		case v1alpha1.PackageRevisionLifecycleProposed:
-			pr.Spec.Lifecycle = v1alpha1.PackageRevisionLifecycleDraft
-			if err := r.client.Update(r.ctx, pr); err != nil {
+			if err := porch.UpdatePackageRevisionApproval(r.ctx, r.client, client.ObjectKey{
+				Namespace: namespace,
+				Name:      name,
+			}, v1alpha1.PackageRevisionLifecycleDraft); err != nil {
 				messages = append(messages, err.Error())
 				fmt.Fprintf(r.Command.ErrOrStderr(), "%s failed (%s)\n", name, err)
 			} else {
