02b5d79d3b540553a3590cd8382d84b2a569bff1
liamfallon <liam.fallon@est.tech>
2024-10-13 21:05:15 +0100

    Pushing a package with no changes in resources deletes all resources in package

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1052,7 +1052,7 @@ func applyResourceMutations(ctx context.Context, draft repository.PackageDraft, 
 		updatedResources, taskResult, err := m.Apply(ctx, baseResources)
 		if taskResult == nil && err == nil {
 			// a nil taskResult means nothing changed
-			continue
+			return baseResources, nil, nil
 		}
 
 		var task *api.Task
