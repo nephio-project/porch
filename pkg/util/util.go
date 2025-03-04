9645c943dfabbc7973d3006a44a651ae1f858218
liamfallon <liam.fallon@est.tech>
2025-03-04 22:09:04 +0000

    Refactoring to fix sonarqube issues

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -83,18 +83,20 @@ func SchemaToMetaGVR(gvr schema.GroupVersionResource) metav1.GroupVersionResourc
 }
 
 func ParseRevisionName(name string) ([]string, error) {
+	const twoDotErrMsg = "malformed package revision name; expected at least two dots: %q"
+
 	firstDot := strings.Index(name, ".")
 	if firstDot < 0 {
-		return nil, fmt.Errorf("malformed package revision name; expected at least two dots: %q", name)
+		return nil, fmt.Errorf(twoDotErrMsg, name)
 	}
 
 	lastDot := strings.LastIndex(name, ".")
 	if lastDot < 0 {
-		return nil, fmt.Errorf("malformed package revision name; expected at least two dots: %q", name)
+		return nil, fmt.Errorf(twoDotErrMsg, name)
 	}
 
 	if firstDot >= lastDot {
-		return nil, fmt.Errorf("malformed package revision name; expected at least two dots: %q", name)
+		return nil, fmt.Errorf(twoDotErrMsg, name)
 	}
 
 	parsedName := make([]string, 3)
