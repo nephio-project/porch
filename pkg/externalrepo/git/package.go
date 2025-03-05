0e3e1002654e16327566936d86aade5235df7018
liamfallon <liam.fallon@est.tech>
2025-03-05 08:55:51 +0000

    Correct misleading comment

--- pkg/externalrepo/git/package.go
+++ pkg/externalrepo/git/package.go
@@ -74,7 +74,7 @@ func (p *gitPackageRevision) KubeObjectName() string {
 		s = string(p.Key().WorkspaceName)
 	}
 
-	// Delete all leading and trailing dots and replace slashes with dots
+	// Replace slashes with dots
 	dottedPath := strings.ReplaceAll(p.path, "/", ".")
 
 	return p.repo.name + "." + dottedPath + "." + s
