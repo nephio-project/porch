45734c47a3b988954a4e5b5509a799138826f8ed
ezmcdja <james.j.mcdermott@ericsson.com>
2025-03-12 10:11:16 +0000

    Small Sonar fix in touched clone_test

--- pkg/task/clone_test.go
+++ pkg/task/clone_test.go
@@ -16,10 +16,11 @@ package task
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
 	"io"
 	"io/fs"
-	"math/rand"
+	"math/big"
 	"net"
 	"net/http"
 	"os"
@@ -189,9 +190,11 @@ func (c *credential) ToAuthMethod() transport.AuthMethod {
 
 func randomString(n int) string {
 	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	lettersLength := big.NewInt(62)
 	result := make([]byte, n)
 	for i := range result {
-		result[i] = letters[rand.Intn(len(letters))]
+		randomLetterIndex, _ := rand.Int(rand.Reader, lettersLength)
+		result[i] = letters[randomLetterIndex.Int64()]
 	}
 	return string(result)
 }
