2f75c25e66f7bd9d415068a45f7ba70e91d4fb30
liamfallon <liam.fallon@est.tech>
2025-03-24 16:13:26 +0000

    Add meta update into SetMeta()  function

--- pkg/cache/crcache/packagerevision.go
+++ pkg/cache/crcache/packagerevision.go
@@ -22,6 +22,7 @@ import (
 	"github.com/nephio-project/porch/pkg/repository"
 	"github.com/nephio-project/porch/pkg/util"
 	"go.opentelemetry.io/otel/trace"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
@@ -77,4 +78,13 @@ func (c *cachedPackageRevision) GetPackageRevision(ctx context.Context) (*api.Pa
 	}
 
 	return apiPR, nil
+}
+
+func (c *cachedPackageRevision) SetMeta(ctx context.Context, pkgRevMeta metav1.ObjectMeta) error {
+
+	if storedPkgRevMeta, err := c.metadataStore.Update(ctx, pkgRevMeta); err == nil {
+		return c.PackageRevision.SetMeta(ctx, storedPkgRevMeta)
+	} else {
+		return err
+	}
 }
