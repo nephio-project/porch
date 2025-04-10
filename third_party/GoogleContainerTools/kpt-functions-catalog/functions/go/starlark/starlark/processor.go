6089babb4ac5e58535de4f919abb7a8b93427fc4
liamfallon <liam.fallon@est.tech>
2025-04-10 17:38:15 +0100

    Merge conflict eror fixed

--- third_party/GoogleContainerTools/kpt-functions-catalog/functions/go/starlark/starlark/processor.go
+++ third_party/GoogleContainerTools/kpt-functions-catalog/functions/go/starlark/starlark/processor.go
@@ -1,39 +1,25 @@
-/*
- Copyright 2025 The Nephio Authors.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- You may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-package cache
+package starlark
 
 import (
-	"context"
-	"testing"
-
-	cachetypes "github.com/nephio-project/porch/pkg/cache/types"
-	"github.com/stretchr/testify/assert"
+	"github.com/nephio-project/porch/third_party/GoogleContainerTools/kpt-functions-sdk/go/fn"
 )
 
-func TestCreateCacheImpl(t *testing.T) {
-	cacheOptions := cachetypes.CacheOptions{
-		CacheType: cachetypes.CRCacheType,
+func Process(resourceList *fn.ResourceList) (bool, error) {
+	err := func() error {
+		sr := &StarlarkRun{}
+		if err := sr.Config(resourceList.FunctionConfig); err != nil {
+			return err
+		}
+		return sr.Transform(resourceList)
+	}()
+	if err != nil {
+		resourceList.Results = []*fn.Result{
+			{
+				Message:  err.Error(),
+				Severity: fn.Error,
+			},
+		}
+		return false, nil
 	}
-
-	cache, err := CreateCacheImpl(context.TODO(), cacheOptions)
-	assert.True(t, err == nil)
-	assert.Equal(t, 0, len(cache.GetRepositories(context.TODO())))
-
-	cacheOptions.CacheType = "BAD-CACHE"
-	_, err = CreateCacheImpl(context.TODO(), cacheOptions)
-	assert.True(t, err != nil)
+	return true, nil
 }
