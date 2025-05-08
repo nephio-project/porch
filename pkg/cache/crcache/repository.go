5e2c7035b6784b3b15d2898ebadd7f656b88c2c0
liamfallon <liam.fallon@est.tech>
2025-05-08 11:00:56 +0100

    Fix grammar in comment

--- pkg/cache/crcache/repository.go
+++ pkg/cache/crcache/repository.go
@@ -463,7 +463,7 @@ func (r *cachedRepository) Close(ctx context.Context) error {
 	return r.repo.Close(ctx)
 }
 
-// pollForever will continue polling until signal channel is closed or ctx is done.
+// pollForever will continue polling until the signal channel is closed or the ctx is done.
 func (r *cachedRepository) pollForever(ctx context.Context, repoSyncFrequency time.Duration) {
 	for {
 		select {
