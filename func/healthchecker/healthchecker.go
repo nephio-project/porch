0f730cedc2d0dae075f8871e0c25a66f820df92f
Kushal Harish Naidu <kushal.harish.naidu@ericsson.com>
2025-05-22 14:59:12 +0100

    Add comment

--- func/healthchecker/healthchecker.go
+++ func/healthchecker/healthchecker.go
@@ -32,6 +32,7 @@ func NewHealthChecker() *HealthChecker {
 }
 
 func (s *HealthChecker) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	// Only logs if log-level is 3 or higher
 	klog.V(3).Info("Serving the Check request for health check")
 	return &grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
@@ -39,6 +40,7 @@ func (s *HealthChecker) Check(ctx context.Context, req *grpc_health_v1.HealthChe
 }
 
 func (s *HealthChecker) Watch(req *grpc_health_v1.HealthCheckRequest, server grpc_health_v1.Health_WatchServer) error {
+	// Only logs if log-level is 3 or higher
 	klog.V(3).Info("Serving the Watch request for health check")
 	return server.Send(&grpc_health_v1.HealthCheckResponse{
 		Status: grpc_health_v1.HealthCheckResponse_SERVING,
