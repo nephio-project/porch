a5787a699bcafdc0c5f329f5b818e82a68ac0c3e
Rendre Greyling <rendre.greyling@nokia.com>
2025-06-24 16:26:18 +0200

    Fix Watcher objects continually created, but not removed - fixes issue #943 (#268)
    * Remove watchers from list when they are finished
    
    * Add unit tests to watchermanager
    
    * Change to logging message

--- pkg/engine/watchermanager.go
+++ pkg/engine/watchermanager.go
@@ -75,7 +75,20 @@ func (r *watcherManager) WatchPackageRevisions(ctx context.Context, filter repos
 	inserted := false
 	for i, watcher := range r.watchers {
 		if watcher != nil {
-			active += 1
+			//Remove any watchers if they are finished and insert the new watcher in the empty slot of the first removed watcher
+			if err := watcher.isDoneFunction(); err != nil {
+				if !inserted {
+					r.watchers[i] = w
+					inserted = true
+					active += 1
+					klog.Infof("watcher %p finished with: %v and is replaced by watcher %p", watcher, err, w)
+				} else {
+					r.watchers[i] = nil
+					klog.Infof("watcher %p finished with: %v and is removed", watcher, err)
+				}
+			} else {
+				active += 1
+			}
 		} else if !inserted {
 			active += 1
 			r.watchers[i] = w
