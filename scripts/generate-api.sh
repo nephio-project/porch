3b93d1281e0a0bf708ec7b5c898bf8166b8cddf3
liamfallon <liam.fallon@est.tech>
2026-01-06 14:37:31 +0000

    Fix copyright dates
    Signed-off-by: liamfallon <liam.fallon@est.tech>

--- scripts/generate-api.sh
+++ scripts/generate-api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#  Copyright 2025 The Nephio Authors.
+#  Copyright 2025-2026 The Nephio Authors.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
