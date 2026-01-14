fe123224b8948b2f21afddf5ee7f955e24753bff
aravind.est <aravindhan.a@est.tech>
2026-01-14 14:42:48 +0000

    Store the packageresources when mutation pipeline fails
    This change doesn't update the status of the packagerevision or packagerevisionresources
    
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- pkg/task/generictaskhandler.go
+++ pkg/task/generictaskhandler.go
@@ -1,4 +1,4 @@
-// Copyright 2022, 2025 The kpt and Nephio Authors
+// Copyright 2022, 2025-2026 The kpt and Nephio Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -186,7 +186,7 @@ func (th *genericTaskHandler) DoPRResourceMutations(
 		Contents: prevResources.Spec.Resources,
 	}
 
-	appliedResources, _, err := mut.apply(ctx, resources)
+	currentResources, _, err := mut.apply(ctx, resources)
 	if err != nil {
 		return nil, err
 	}
@@ -200,10 +200,13 @@ func (th *genericTaskHandler) DoPRResourceMutations(
 	// and can be amended using the error returned as a reference point to ensure
 	// the package renders properly, before retrying the push.
 	var (
-		renderStatus *porchapi.RenderStatus
-		renderResult *porchapi.TaskResult
+		appliedResources repository.PackageResources
+		renderStatus     *porchapi.RenderStatus
+		renderResult     *porchapi.TaskResult
 	)
-	appliedResources, renderResult, err = th.renderMutation(oldRes.GetNamespace()).apply(ctx, appliedResources)
+	prr := &porchapi.PackageRevisionResources{}
+
+	appliedResources, renderResult, err = th.renderMutation(oldRes.GetNamespace()).apply(ctx, currentResources)
 	// keep last render result on empty patch
 	if renderResult != nil &&
 		renderResult.RenderStatus != nil &&
@@ -212,14 +215,14 @@ func (th *genericTaskHandler) DoPRResourceMutations(
 		renderStatus = renderResult.RenderStatus
 	}
 	if err != nil {
-		klog.Error(err)
-		return renderStatus, renderError(err)
-	}
-
-	prr := &porchapi.PackageRevisionResources{
-		Spec: porchapi.PackageRevisionResourcesSpec{
+		klog.Error(renderError(err))
+		prr.Spec = porchapi.PackageRevisionResourcesSpec{
+			Resources: currentResources.Contents,
+		}
+	} else {
+		prr.Spec = porchapi.PackageRevisionResourcesSpec{
 			Resources: appliedResources.Contents,
-		},
+		}
 	}
 
 	return renderStatus, draft.UpdateResources(ctx, prr, &porchapi.Task{Type: porchapi.TaskTypeRender})
